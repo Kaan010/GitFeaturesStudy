37b273a4ed000e255258ec28abb25206675f6e0f
Kaan010 <kaankalan@outlook.com.tr>
2022-01-11 13:32:05 +0300

    Revert "deneme1.4"
    This reverts commit d8dc4fc504e8dcdb7f35407bf4aaf29a183e371c.

--- src/main/java/main1.java
+++ src/main/java/main1.java
@@ -11,7 +11,4 @@ public class main1 {
     public void function3(){
         System.out.println("function3");
     }
-    public void function4(){
-        System.out.println("function4");
-    }
 }

375c9ab1fb54a8e25413f3f3698c5e41698880e6
Kaan010 <kaankalan@outlook.com.tr>
2022-01-11 12:34:03 +0300

    deneme1.2

--- src/main/java/main1.java
+++ src/main/java/main1.java
@@ -2,4 +2,10 @@ public class main1 {
     public static void main(String[] args) {
         System.out.println("bas kocum");
     }
+    public void function1(){
+        System.out.println("function1");
+    }
+    public void function2(){
+        System.out.println("function2");
+    }
 }
